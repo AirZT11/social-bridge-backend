a06e712facdc08960146223b00ea2f110f4486b8
Sam Kim <samjkim11@gmail.com>
2023-04-14 16:12:17 -0400

    Commit remaining files and add .gitignore

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ import postRoutes from "./routes/posts.js"
 import { createPost } from "./controllers/posts.js"
 import { register } from "./controllers/auth.js"
 import { verifyToken } from "./middleware/auth.js"
+import User from "./models/User.js"
+import Post from "./models/Post.js"
+import { users, posts } from "./data/index.js"
 
 /* MIDDLEWARE CONFIGURATIONS */
 // Use this to grab file url, specifically for modules use case
@@ -67,7 +70,11 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() =>
+  .then(() => {
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
-  )
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users)
+    // Post.insertMany(posts)
+  })
   .catch((error) => console.log(error.message))
